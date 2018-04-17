5dfc1c19adbe30e9c3bde24c6235bc728c0495f6
Ellie Keen <elliekeen95@gmail.com>
2018-04-17 21:44:26 +0100

    Unit test for releasing a bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,8 @@ require 'docking_station'
 
 describe DockingStation do
 
-it 'should allow DockingStation to "Release a bike"'
+    it 'should allow DockingStation to "release_bike"' do
+    expect(DockingStation.new).to respond_to(:release_bike)
 
-
+    end
 end
