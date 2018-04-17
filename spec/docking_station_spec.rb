69c64e3759cdbc6c60d78f9d4dd447474dfca6b5
Ellie Keen <elliekeen95@gmail.com>
2018-04-17 21:52:04 +0100

    Re-wrote test using one-line syntax

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,8 +2,6 @@ require 'docking_station'
 
 describe DockingStation do
 
-    it 'should allow DockingStation to "release_bike"' do
-    expect(DockingStation.new).to respond_to(:release_bike)
+    it {expect(DockingStation.new).to respond_to(:release_bike)}
 
-    end
 end
